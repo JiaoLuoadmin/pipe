6de5f905a233ca18f64bb2881827beeb746268a2
荒野無燈 <i@80x86.io>
2018-03-24 15:18:38 +0800

    build.sh: use yarn instead of npm

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-go build -i -v
-cd console && npm install && npm run build
-cd ../theme && npm install && npm install --global gulp && gulp
+dep ensure
+go build -ldflags '-s -w' -v
+cd console && yarn install && yarn run build
+cd ../theme && yarn install && yarn global add gulp && gulp
 
 echo 'build pipe done'
